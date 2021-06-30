96be6dbc9324857fed7e2ded61ac718c2151f695
aaamra <dev.aaamra@gmail.com>
2021-06-30 15:14:07 +0300

    create score page and finish the project

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 const startNowButton = document.querySelector('#start-now-button');
 const nameInput = document.querySelector('#name-input');
+const leadboardUl = document.querySelector('#leadboard-ul');
+
+const leadboard = JSON.parse(localStorage.getItem('leadboard')) || [];
+
+document.addEventListener('DOMContentLoaded',() => {
+    leadboardUl.innerHTML = "";
+    leadboard.forEach(user => {
+        const span = document.createElement('span');
+        span.textContent = `${user.score} / 10`;
+
+        const li = document.createElement('li');
+        li.textContent = `${user.name}`;
+        li.appendChild(span);
 
 
+        leadboardUl.appendChild(li);
+    });
+});
 
 startNowButton.addEventListener('click',(e) => {
     e.preventDefault();
